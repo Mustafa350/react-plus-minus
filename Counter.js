727d1ee206b205794c984fa60759220605e2cbd2
Mustafa Ertan <46426293+Mustafa350@users.noreply.github.com>
2019-03-17 22:46:14 +0300

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './style.css';
 
 class Counter extends Component {
 state = {
@@ -15,17 +15,35 @@ artı = () => {
     number: ++this.state.number
   });
 }
+topla = () => {
+  this.setState({
+    number: -+this.state.number
+  });
+}
+toplas = () => {
+  this.setState({
+     number: 0
+
+  });
+}
+
  render(){
    return(
    <div>
    <h1>{this.state.number}</h1>
-   <button onClick={this.artı}>+</button>
-   <button  onClick={this.eksi}>-</button>
+   <button className="button1" onClick={this.artı}>+</button>
+   <button className="button2" onClick={this.eksi}>-</button>
+   <button className="button3" onClick={this.topla}>Sayıları Eksi Yap</button>
+   <button className="button4" onClick={this.toplas}>Sayıları Sıfırla</button>
+
+
     </div>
 
 
   );
  }
 }
+
+
 
 export default Counter;
